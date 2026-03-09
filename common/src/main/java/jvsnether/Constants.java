464802c2008352ec191bafc96662dda223308e04
TheGoldenWorld <dawnoftime.inc@gmail.com>
2026-03-09 11:19:24 +0100

    Basis of the mod

---
+++ common/src/main/java/jvsnether/Constants.java
@@ -0,0 +1,11 @@
+package jvsnether;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "jvsnether";
+	public static final String MOD_NAME = "jvsnether";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
